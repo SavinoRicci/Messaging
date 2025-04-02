1ddd2a44255ba0640fedf61c1c81908edd10798b
SavinoRicci <savino.ricci@computacenter.com>
2025-04-02 16:07:20 +0100

    test

---
+++ test.tf
@@ -0,0 +1,20 @@
+terraform {
+  cloud {
+    organization = "Computacenter-GIS-Genesys"
+    workspaces {
+      name = "cceudev1"
+    }
+  }
+  required_providers {
+    genesyscloud = {
+      source  = "mypurecloud/genesyscloud"
+      version = "~> 1.48.0"
+    }
+  }
+}
+
+provider "genesyscloud" {
+  oauthclient_id     = "var.clientId"
+  oauthclient_secret = "var.clientSecret"
+  aws_region         = "eu-central-1"
+}
